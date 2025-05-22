bcfe12c5bce831484776992ebcf0255e0268b203
Denys <dgolub.ai@gmail.com>
2025-05-22 23:17:19 +0300

    first commit

---
+++ Lab4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int j = 0; j < 50; j++)
+            {
+                Table table = new Table();
+                for (int i = 0; i < 5; i++)
+                {
+                    new Philosopher(i, table);
+                }
+            }
+        }
+    }
+}
---
+++ Lab4/Waiter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public class Waiter
+    {
+        private Semaphore waiter;
+
+        public Waiter(int numWaiters)
+        {
+            waiter = new Semaphore(numWaiters, numWaiters);
+        }
+
+        public void GetWaitersAttention()
+        {
+            waiter.WaitOne();
+        }
+
+        public void ReleaseWaitersAttention()
+        {
+            waiter.Release();
+        }
+    }
+
+}
